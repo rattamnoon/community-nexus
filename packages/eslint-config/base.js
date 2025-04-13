3691f1b7062e5350f12269f510919034ce680e0e
UN <rattamnoon.kir@gmail.com>
2025-04-13 14:15:13 +0700

    :truck: chore(pkg/eslint-config): setting eslint api and web

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,25 +1,23 @@
 /** @type {import("eslint").Linter.Config} */
-const turboConfig = require("eslint-config-turbo").default;
 
 module.exports = {
   root: true,
-  ...turboConfig,
   extends: [
-    ...turboConfig.extends,
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
+    'eslint-config-turbo',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  plugins: [...(turboConfig.plugins || []), "@typescript-eslint/eslint-plugin"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  parser: '@typescript-eslint/parser',
   ignorePatterns: [
-    ".*.js",
-    "*.setup.js",
-    "*.config.js",
-    ".turbo/",
-    "dist/",
-    "coverage/",
-    "node_modules/",
-    ".husky/",
+    '.*.js',
+    '*.setup.js',
+    '*.config.js',
+    '.turbo/',
+    'dist/',
+    'coverage/',
+    'node_modules/',
+    '.husky/',
   ],
 };
