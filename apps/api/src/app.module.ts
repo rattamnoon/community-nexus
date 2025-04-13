56ba30d2c54f9ecd67168262efe7b60014b7a043
UN <rattamnoon.kir@gmail.com>
2025-04-13 14:27:36 +0700

    :truck: chore(pkg/api): config api and env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { LinksModule } from './links/links.module';
 
-import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [LinksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LinksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 4000;
+
+  await app.listen(port);
 }
 bootstrap();
